3d41fe6ecdb1af5cb95390142e36b7440246c6eb
Grigoriev Oleg <go.vasac@gmail.com>
2010-12-12 15:50:25 +0300

    Адаптер "тестового" типа СУБД

---
+++ goDB/Adapters/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Адаптер "тестового" типа СУБД
+ *
+ * @package    go\DB
+ * @subpackage Adapters
+ * @author     Григорьев Олег aka vasa_c
+ */
+
+namespace go\DB\Adapters;
+
+final class test extends \go\DB\DB
+{
+    
+}
