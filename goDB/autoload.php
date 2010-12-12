90f6f47cf0977816f96034938d19c0e4aa4a7080
Grigoriev Oleg <go.vasac@gmail.com>
2010-12-12 13:17:06 +0300

    Автозагрузка классов

---
+++ goDB/autoload.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Автозагрузка классов в goDB
+ *
+ * @example
+ * <code>
+ * require_once('/path/to/goDB/autoload.php');
+ * spl_autoload_register('\go\DB\autoload');
+ * </code>
+ *
+ * @package go\DB
+ * @author  Григорьев Олег aka vasa_c (http://blgo.ru/)
+ */
+
+namespace go\DB;
+
+/**
+ * Загрузить класс по имени
+ * 
+ * @param string $classname
+ *        имя требуемого класса
+ * @return bool
+ *         был ли загружен класс (FALSE - класс не найден или не относится к go\DB)
+ */
+function autoload($classname) {
+    if (\strpos($classname, __NAMESPACE__) !== 0) {
+        return false;
+    }
+    $localname = substr($classname, strlen(__NAMESPACE__));
+    $filename  = __DIR__.str_replace('\\', '/', $localname.'.php');
+    if (!file_exists($filename)) {
+        return false;
+    }
+    require_once($filename);
+    return class_exists($classname, false);
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+header('Content-Type: text/html; charset=utf-8');
+
+echo '<h1>go\DB 2</h1>';
+
+require(__DIR__.'/goDB/autoload.php');
+
+spl_autoload_register('\go\DB\autoload');
