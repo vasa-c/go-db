2d6b6c14683ac2a62756935e3024b4b0ebe45324
Alex Polev <alex.v.polev@gmail.com>
2013-06-16 22:56:48 +0400

    [fix] code style

--- goDB/Implementations/pgsql.php
+++ goDB/Implementations/pgsql.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *	Надстройка над php_pgsql
+ *    Надстройка над php_pgsql
  *
  * @package  go\DB
  * @subpackage Implementations
@@ -11,14 +11,14 @@ namespace go\DB\Implementations;
 
 final class pgsql extends Base
 {
-	/**
+    /**
      * Обязательные параметры подключения
      *
      * @var array
      */
     protected $paramsReq = array();
 
-	/**
+    /**
      * Необязательные параметры подключения
      *
      * параметр => значение по умолчанию
@@ -26,16 +26,16 @@ final class pgsql extends Base
      * @var array
      */
     protected $paramsDefault = array(
-		'username'        => null,
-		'password'        => null,
-        'dbname'          => null,
-        'charset'         => null,
-		'port'            => null,
-		'hostaddr'        => null,
-		'connect_timeout' => null,
-		'options'         => null,
-		'sslmode'         => null,
-		'service'         => null,
+        'username' => null,
+        'password' => null,
+        'dbname' => null,
+        'charset' => null,
+        'port' => null,
+        'hostaddr' => null,
+        'connect_timeout' => null,
+        'options' => null,
+        'sslmode' => null,
+        'service' => null,
     );
 
 
@@ -48,35 +48,35 @@ final class pgsql extends Base
      * @return mixed
      */
     public function connect(array $params, &$errorInfo = null, &$errorCode = null) {
-		if(isset ($params['host'])){
-			$host = \explode(':', $params['host'], 2);
-			if (!empty($host[1])) {
-				$params['host'] =  $host[0];
-				$params['port'] =  $host[1];
-			}
-		}
+        if (isset ($params['host'])) {
+            $host = \explode(':', $params['host'], 2);
+            if (!empty($host[1])) {
+                $params['host'] = $host[0];
+                $params['port'] = $host[1];
+            }
+        }
 
         $connection = @\pg_connect($this->generateConnectString($params), PGSQL_CONNECT_FORCE_NEW);
 
-		if(!$connection){
-			$errorInfo = \error_get_last();
-			return false;
-		}
+        if (!$connection) {
+            $errorInfo = \error_get_last();
+            return false;
+        }
 
         return $connection;
     }
 
 
-	/**
-	 * @override Base
-	 * @param mixed $connection
-	 * @return bool
-	 */
-	public function close($connection) {
+    /**
+     * @override Base
+     * @param mixed $connection
+     * @return bool
+     */
+    public function close($connection) {
         return @\pg_close($connection);
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -95,29 +95,29 @@ final class pgsql extends Base
      * @return mixed
      */
     public function getInsertId($connection, $cursor = null) {
-		$result  =  @\pg_query($connection, 'SELECT lastval()');
+        $result = @\pg_query($connection, 'SELECT lastval()');
 
-		if(!$result){
-			return  false;
-		}
+        if (!$result) {
+            return false;
+        }
 
-		$row  =  pg_fetch_row($result);
+        $row = pg_fetch_row($result);
 
-		return  $row[0];
+        return $row[0];
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
      * @param mixed $cursor [optional]
      * @return int
-	 */
-	public  function  getAffectedRows($connection, $cursor = null) {
-		return \pg_affected_rows($cursor);
-	}
+     */
+    public function  getAffectedRows($connection, $cursor = null) {
+        return \pg_affected_rows($cursor);
+    }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -136,10 +136,10 @@ final class pgsql extends Base
      * @return int
      */
     public function getErrorCode($connection, $cursor = null) {
-        return  null;
+        return null;
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -150,7 +150,7 @@ final class pgsql extends Base
         return \pg_numrows($cursor);
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -172,7 +172,7 @@ final class pgsql extends Base
         return \pg_fetch_assoc($cursor);
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -184,17 +184,17 @@ final class pgsql extends Base
     }
 
 
-	/**
-	 * @override Base
-	 * @param mixed $connection
-	 * @param mixed $cursor
-	 * @return bool
-	 */
-	public function freeCursor($connection, $cursor) {
+    /**
+     * @override Base
+     * @param mixed $connection
+     * @param mixed $cursor
+     * @return bool
+     */
+    public function freeCursor($connection, $cursor) {
         return \pg_free_result($cursor);
     }
 
-	/**
+    /**
      * @override Base
      *
      * @param mixed $connection
@@ -213,7 +213,7 @@ final class pgsql extends Base
      * @return string
      */
     public function reprString($connection, $value) {
-        return '\''.$this->escapeString($connection, $value).'\'';
+        return '\'' . $this->escapeString($connection, $value) . '\'';
     }
 
     /**
@@ -224,54 +224,54 @@ final class pgsql extends Base
      * @return string
      */
     protected function reprField($connection, $value) {
-        return '"'.$value.'"';
+        return '"' . $value . '"';
     }
 
-	/**
-	 * @override Base
-	 * @param mixed $connection
-	 * @param mixed $cursor
-	 * @return bool
-	 */
-	public function rewindCursor($connection, $cursor) {
+    /**
+     * @override Base
+     * @param mixed $connection
+     * @param mixed $cursor
+     * @return bool
+     */
+    public function rewindCursor($connection, $cursor) {
         return \pg_result_seek($cursor, 0);
     }
 
     /**
      * Генерируем строку для подключения к БД
      *
-     * @param array  $params параметры для подключения (@see $this->paramsDefault)
+     * @param array $params параметры для подключения (@see $this->paramsDefault)
      *
      * @return  String
      */
-    private  function  generateConnectString($params){
+    private function  generateConnectString($params) {
 
-        $connString  =  '';
-        if($params){
-            foreach($params  as  $key=>$value){
+        $connString = '';
+        if ($params) {
+            foreach ($params as $key => $value) {
 
-                if(!$value){
+                if (!$value) {
                     continue;
                 }
 
-                switch($key){
+                switch ($key) {
                     case 'username':
-                        $connString  .=  'user='.$value;
-                    break;
+                        $connString .= 'user=' . $value;
+                        break;
 
-					case 'charset':
-						$connString  .=  'options=\'--client_encoding='.$value.'\'';
-					break;
+                    case 'charset':
+                        $connString .= 'options=\'--client_encoding=' . $value . '\'';
+                        break;
                     default:
-                        $connString  .=  $key.'='.$value;
-                    break;
+                        $connString .= $key . '=' . $value;
+                        break;
                 }
 
-                $connString  .=  ' ';
+                $connString .= ' ';
             }
         }
 
-        return  rtrim($connString);
+        return rtrim($connString);
     }
 
 }
