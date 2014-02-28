9b67c9132e3465d138a18d0c72bfb3fbdecf1397
Oleg Grigoriev <go.vasac@gmail.com>
2014-02-28 18:42:30 +0400

    Helpers\MapFields: The map of fields to columns

---
+++ goDB/Helpers/MapFields.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * @package go\DB
+ */
+
+namespace go\DB\Helpers;
+
+/**
+ * The map of fields to columns
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+class MapFields
+{
+    /**
+     * Constructor
+     *
+     * @param array $map
+     */
+    public function __construct(array $map)
+    {
+        $this->map = $map;
+    }
+
+    /**
+     * @param string $col
+     * @return string
+     */
+    public function col($col)
+    {
+        if (\is_string($col) && (isset($this->map[$col]))) {
+            return $this->map[$col];
+        }
+        return $col;
+    }
+
+    /**
+     * @param mixed $cols
+     * @return mixed
+     */
+    public function cols($cols)
+    {
+        if (\is_array($cols)) {
+            foreach ($cols as &$col) {
+                if (\is_string($col) && (isset($this->map[$col]))) {
+                    $col = $this->map[$col];
+                }
+            }
+            unset($col);
+        }
+        return $cols;
+    }
+
+    /**
+     * @param mixed $set
+     * @return mixed
+     */
+    public function set($set)
+    {
+        $result = array();
+        foreach ($set as $k => $v) {
+            if (isset($this->map[$k])) {
+                $k = $this->map[$k];
+            }
+            if (isset($v['col']) && (isset($this->map[$v['col']]))) {
+                $v['col'] = $this->map[$v['col']];
+            }
+            $result[$k] = $v;
+        }
+        return $result;
+    }
+
+    /**
+     * @param mixed $where
+     * @return mixed
+     */
+    public function where($where)
+    {
+        if (!\is_array($where)) {
+            return $where;
+        }
+        $result = array();
+        foreach ($where as $k => $v) {
+            if (isset($this->map[$k])) {
+                $k = $this->map[$k];
+            }
+            if (isset($v['col']) && (isset($this->map[$v['col']]))) {
+                $v['col'] = $this->map[$v['col']];
+            }
+            $result[$k] = $v;
+        }
+        return $result;
+    }
+
+    /**
+     * @param mixed $order
+     * @return mixed
+     */
+    public function order($order)
+    {
+        if (\is_string($order)) {
+            return isset($this->map[$order]) ? $this->map[$order] : $order;
+        }
+        if (!\is_array($order)) {
+            return null;
+        }
+        $result = array();
+        foreach ($order as $k => $v) {
+            if (\is_int($k)) {
+                $v = isset($this->map[$v]) ? $this->map[$v] : $v;
+            } else {
+                $k = isset($this->map[$k]) ? $this->map[$k] : $k;
+            }
+            $result[$k] = $v;
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $rows
+     * @return array
+     */
+    public function assoc($rows)
+    {
+        if (!$this->flip) {
+            $this->flip = \array_flip($this->map);
+        }
+        $result = [];
+        foreach ($rows as $row) {
+            $item = [];
+            foreach ($row as $k => $v) {
+                $k = isset($this->flip[$k]) ? $this->flip[$k] : $k;
+                $item[$k] = $v;
+            }
+            $result[] = $item;
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $row
+     * @return array
+     */
+    public function row($row)
+    {
+        if (!\is_array($row)) {
+            return $row;
+        }
+        if (!$this->flip) {
+            $this->flip = \array_flip($this->map);
+        }
+        $item = [];
+        foreach ($row as $k => $v) {
+            $k = isset($this->flip[$k]) ? $this->flip[$k] : $k;
+            $item[$k] = $v;
+        }
+        return $item;
+    }
+
+    /**
+     * @var array
+     */
+    protected $map;
+
+    /**
+     * @var array
+     */
+    protected $flip;
+}
---
+++ tests/Helpers/MapFieldsTest.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @package go\DB
+ */
+
+namespace go\Tests\DB\Helpers;
+
+use go\DB\Helpers\MapFields;
+
+/**
+ * @coversDefaultClass go\DB\Helpers\MapFields
+ */
+class MapFieldsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var go\DB\Helpers\MapFields
+     */
+    private $map = [
+        'one' => 'o',
+        'two' => 't',
+    ];
+
+    /**
+     * @covers ::col
+     * @covers ::cols
+     * @covers ::set
+     * @covers ::where
+     * @covers ::assoc
+     * @covers ::row
+     * @dataProvider provider
+     * @param mixed $value
+     * @param mixed $expected
+     */
+    public function test($method, $value, $expected)
+    {
+        $map = new MapFields($this->map);
+        $this->assertEquals($expected, $map->$method($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function provider()
+    {
+        return [
+            [
+                'col',
+                'one',
+                'o',
+            ],
+            [
+                'col',
+                'three',
+                'three',
+            ],
+            [
+                'col',
+                ['t', 'one'],
+                ['t', 'one'],
+            ],
+            [
+                'cols',
+                null,
+                null,
+            ],
+            [
+                'cols',
+                ['one', ['t', 'two'], 'three'],
+                ['o', ['t', 'two'], 'three'],
+            ],
+            [
+                'set',
+                ['one' => 1, 'two' => 2, 'three' => 3],
+                ['o' => 1, 't' => 2, 'three' => 3],
+            ],
+            [
+                'set',
+                ['one' => 1, 'two' => 2, 'three' => 3],
+                ['o' => 1, 't' => 2, 'three' => 3],
+            ],
+            [
+                'set',
+                ['one' => ['col' => 'one', 'value' => 1], 'two' => ['value' => 2]],
+                ['o' => ['col' => 'o', 'value' => 1], 't' => ['value' => 2]],
+            ],
+            [
+                'where',
+                true,
+                true,
+            ],
+            [
+                'where',
+                ['one' => 1, 'two' => [1, 2, 3]],
+                ['o' => 1, 't' => [1, 2, 3]],
+            ],
+            [
+                'where',
+                ['one' => ['op' => '=', 'col' => 'two', 'value' => 3], 'three' => [1, 2, 3]],
+                ['o' => ['op' => '=', 'col' => 't', 'value' => 3], 'three' => [1, 2, 3]],
+            ],
+            [
+                'order',
+                null,
+                null,
+            ],
+            [
+                'order',
+                'one',
+                'o',
+            ],
+            [
+                'order',
+                'three',
+                'three',
+            ],
+            [
+                'order',
+                ['one', 'four', 'two'],
+                ['o', 'four', 't'],
+            ],
+            [
+                'order',
+                ['one', 'four', 'two' => true],
+                ['o', 'four', 't' => true],
+            ],
+            [
+                'assoc',
+                [
+                    ['id' => 1, 'o' => 2, 't' => 3],
+                    ['id' => 4, 'o' => 5, 't' => 6],
+                ],
+                [
+                    ['id' => 1, 'one' => 2, 'two' => 3],
+                    ['id' => 4, 'one' => 5, 'two' => 6],
+                ],
+            ],
+            [
+                'row',
+                ['id' => 1, 'o' => 2, 't' => 3],
+                ['id' => 1, 'one' => 2, 'two' => 3],
+            ],
+            [
+                'row',
+                null,
+                null,
+            ],
+        ];
+    }
+}
