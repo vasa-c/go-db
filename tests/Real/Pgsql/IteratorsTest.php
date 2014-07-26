350da8e3c479682e2e00c5fd11d986173251810f
apolev <alex.v.polev@gmail.com>
2014-07-26 10:05:05 +0400

    Add real test for PostgreSQL

---
+++ tests/Real/Pgsql/IteratorsTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package go\DB
+ */
+
+namespace go\Tests\DB\Real\Pgsql;
+
+use go\Tests\DB\Real\Base;
+
+class IteratorsTest extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $adapter = 'pgsql';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $reqExt = 'pgsql';
+
+    public function testIterators()
+    {
+        $db = $this->createDB(__DIR__.'/dump.sql');
+        $sql = 'SELECT "id","num" FROM "godbtest" ORDER BY "id" ASC';
+        $assoc = $db($sql)->assoc();
+        $it = $db($sql)->iassoc();
+        $this->assertSame($assoc, \iterator_to_array($it));
+        $this->assertSame($db($sql)->numerics(), \iterator_to_array($db($sql)->inumerics()));
+        $this->assertSame($assoc, \iterator_to_array($it));
+        $this->assertSame($db($sql)->col(), \iterator_to_array($db($sql)->icol()));
+        $this->assertSame($db($sql)->vars(), \iterator_to_array($db($sql)->ivars()));
+        $this->assertSame($db($sql)->assoc(), \iterator_to_array($db($sql)));
+        $this->assertCount(5, $db($sql)->iassoc());
+        $this->assertCount(5, $db($sql));
+        $this->assertSame($db($sql)->assoc('id'), \iterator_to_array($db($sql)->iassoc('id')));
+    }
+}
