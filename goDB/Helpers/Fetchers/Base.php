e19f99a2a7399c5faa3483f2662a2936d9a4f4d6
Oleg Grigoriev <go.vasac@gmail.com>
2015-07-27 15:15:19 +0300

    ref

--- goDB/Helpers/Fetchers/Base.php
+++ goDB/Helpers/Fetchers/Base.php
@@ -29,13 +29,12 @@ abstract class Base implements Result
      */
     public function fetch($fetch)
     {
-        $fetch = explode(':', $fetch, 2);
-        $param = isset($fetch[1]) ? strtolower($fetch[1]) : null;
-        $fetch = $fetch[0];
+        $params = explode(':', $fetch);
+        $method = array_shift($params);
         $fetches = Config::get('fetch');
-        if (!isset($fetches[$fetch])) {
-            throw new UnknownFetch($fetch);
+        if (!isset($fetches[$method])) {
+            throw new UnknownFetch($method);
         }
-        return $this->$fetch($param);
+        return call_user_func_array(array($this, $method), $params);
     }
 }
