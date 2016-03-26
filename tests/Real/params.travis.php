240f3a4cda9fc16e96927e8366eaee766197fb6e
Oleg Grigoriev <go.vasac@gmail.com>
2016-03-26 22:23:25 +0300

    Travis-ci: sqlite

---
+++ tests/Real/params.travis.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Connection parameters for travis-ci.org
+ *
+ * @package go\DB
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+return array(
+    'mysql' => null,
+
+    'mysqlold' => null,
+
+    'sqlite' => array(
+        'filename' => ':memory:',
+    ),
+
+    'pgsql' => null,
+);
